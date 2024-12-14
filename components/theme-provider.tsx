a187b950e409d35656e3213c861daa3d26d483a3
yuvamk <103384688+yuvamk@users.noreply.github.com>
2024-12-14 18:48:51 +0530

    fix01

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
 
 // Use React.PropsWithChildren for the component props
-export function ThemeProvider({ children, ...props }: React.PropsWithChildren<any>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: React.PropsWithChildren<ThemeProviderProps>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
